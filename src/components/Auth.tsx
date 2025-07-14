345ecb518de1e3933dd76e7587f03df07d2078c7
takamano <tamano1118@gmail.com>
2025-07-14 21:41:05 +0900

    3rd deployment

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -3,11 +3,11 @@ import { fetchUserAttributes } from 'aws-amplify/auth';
 import { useState, useEffect } from 'react';
 
 import { Amplify } from 'aws-amplify';
-import config from "../../config.json"
 
 
-const userPoolId = import.meta.env.VITE_USER_POOL_ID || config.amplify.userPoolId;
-const userPoolClientId = import.meta.env.VITE_USER_POOL_CLIENT_ID || config.amplify.userPoolClientId;
+
+const userPoolId = import.meta.env.VITE_USER_POOL_ID;
+const userPoolClientId = import.meta.env.VITE_USER_POOL_CLIENT_ID;
 
 Amplify.configure({
     Auth:{
